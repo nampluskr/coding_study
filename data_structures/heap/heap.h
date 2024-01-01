74eb71038ac4c60316cee49d318f913e2235c88a
nampluskr <nampluskr@gmail.com>
2024-01-02 03:52:42 +0900

    update

--- data_structures/heap/heap.h
+++ data_structures/heap/heap.h
@@ -1,22 +1,100 @@
-#pragma once
+﻿#pragma once
+
+template<typename T>
+inline void swap(T& a, T& b) { T temp = a; a = b; b = temp; }
 
 template<typename T, int max_size>
 struct MaxHeap {
     T heap[max_size];
     int heapSize = 0;
 
+    // 기본 함수
     void clear() { heapSize = 0; }
     void push(const T& data);
     void pop();
-    bool empty() { heapSize == 0; }
+    bool empty() { return heapSize == 0; }
     T top() { return heap[1]; }
     int size() { return heapSize; }
+
+    // 추가 함수
+    void siftUp(int cur);
+    void siftDown(int cur);
+    void heapity(const T arr[], int size);
 };
 
 template<typename T, int max_size>
 void MaxHeap<T, max_size>::push(const T& data) {
+    if (heapSize == max_size - 1) return;   // 예외 처리
+
+    heapSize++;
+    heap[heapSize] = data;
+
+    // Sift up
+    int cur = heapSize;
+    int parent = cur / 2;
+    while (parent >= 1) {
+        if (heap[cur] < heap[parent]) break;
+
+        swap(heap[parent], heap[cur]);
+        cur = parent;
+        parent = cur / 2;
+    }
 }
 
 template<typename T, int max_size>
 void MaxHeap<T, max_size>::pop() {
+    if (heapSize == 0) return;      // 예외 처리
+
+    swap(heap[1], heap[heapSize]);
+    heapSize--;
+
+    // Sift down
+    siftDown(1);
+    int cur = 1;
+    int child = cur * 2;
+    while (child + 1 <= heapSize) {
+        if (heap[child] < heap[child + 1]) child++;
+        if (heap[child] < heap[cur]) break;
+
+        swap(heap[cur], heap[child]);
+        cur = child;
+        child = cur * 2;
+    }
+}
+
+template<typename T, int max_size>
+void MaxHeap<T, max_size>::siftUp(int cur) {
+    int cur = heapSize;
+    int parent = cur / 2;
+    while (parent >= 1) {
+        if (heap[cur] < heap[parent]) break;
+
+        swap(heap[parent], heap[cur]);
+        cur = parent;
+        parent = cur / 2;
+    }
+}
+
+template<typename T, int max_size>
+void MaxHeap<T, max_size>::siftDown(int cur) {
+    int cur = 1;
+    int child = cur * 2;
+    while (child + 1 <= heapSize) {
+        if (heap[child] < heap[child + 1]) child++;
+        if (heap[child] < heap[cur]) break;
+
+        swap(heap[cur], heap[child]);
+        cur = child;
+        child = cur * 2;
+    }
+}
+
+template<typename T, int max_size>
+void MaxHeap<T, max_size>::heapity(const T arr[], int size) {
+    // deep copy
+    for (int i = 0; i < size; i++)
+        heap[1 + i] = arr[i];
+    // heapity
+    for (int i = size / 2; i >= 1; i--)
+        siftDown(i);
 }
--- data_structures/heap/test_heap.cpp
+++ data_structures/heap/test_heap.cpp
@@ -1,7 +1,28 @@
 #include <cstdio>
 #include "heap.h"
 
+
+struct Data {
+    int value, idx;
+
+    bool operator<(const Data& data) const {
+        return (value < data.value) || (value == data.value && idx > data.idx);
+    }
+};
+
+MaxHeap<Data, 10> PQ;
+
 int main()
 {
+    PQ.push({ 10, 1 });
+    PQ.push({ 30, 3 });
+    PQ.push({ 20, 2 });
+    PQ.push({ 40, 4 });
+    PQ.push({ 50, 5 });
+
+    while (!PQ.empty())
+        PQ.pop();
+
+
     return 0;
 }
